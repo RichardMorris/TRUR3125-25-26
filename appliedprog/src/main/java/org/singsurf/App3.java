2286d119b0a1026320d7d71023ae91fd4d9738e0
Richard Morris <rich@singsurf.org>
2025-10-06 09:21:53 +0000

    App3

---
+++ appliedprog/src/main/java/org/singsurf/App3.java
@@ -0,0 +1,20 @@
+package org.singsurf;
+
+/**
+ * Hello world!
+ */
+public class App3 {
+    String message; // Define a field 
+    public App3(String txt) { // A constructor
+        this.message = txt;
+    }
+
+    public void welcome(String name) { // A method
+        System.out.println("Hello "+name+"!");
+    }
+
+    public static void main(String[] args) {
+        App3 myapp = new App3("Hello world!"); // build the app
+        myapp.welcome("Rich"); // call the method
+    }
+}
